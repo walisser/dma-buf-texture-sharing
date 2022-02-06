98023fb626bbadd65a933ffe27ac800f8c7d4c96
Blaž Tomažič <blaz.tomazic@gmail.com>
2022-02-06 14:33:22 +0100

    Remove uneeded flush

--- main.c
+++ main.c
@@ -85,8 +85,8 @@ int main(int argc, char **argv)
         assert(image != EGL_NO_IMAGE);
         
         // The next line works around an issue in radeonsi driver (fixed in master at the time of writing). If you are
-        // not having problems with texture rendering until the first texture update you can omit this line
-        glFlush();
+        // having problems with texture rendering until the first texture update you can uncomment this line
+        // glFlush();
 
         // EGL (extension: EGL_MESA_image_dma_buf_export): Get file descriptor (texture_dmabuf_fd) for the EGL image and get its
         // storage data (texture_storage_metadata - fourcc, stride, offset)
